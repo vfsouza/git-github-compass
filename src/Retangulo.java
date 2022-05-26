4ab52431d8778f4e3e3c284f5778dec48fc30783
vfsouza <vini.f.souza@hotmail.com>
2022-05-26 03:12:01 -0300

    Feat: Classe quadrado

---
+++ src/Retangulo.java
@@ -0,0 +1,28 @@
+public class Retangulo {
+    private float lado;
+    private float altura;
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float area() {
+        return lado * altura;
+    }
+
+    public float perimetro() {
+        return (lado * 2) + (altura * 2);
+    }
+}
